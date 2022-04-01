501138f64d399a33d584382c71dea24817cbfc22
Bsubs <bsubs02@gmail.com>
2022-04-02 01:51:30 +0800

    Begin conversion from pure html files to Flask app

---
+++ cartographers/app.py
@@ -0,0 +1,19 @@
+from distutils.log import info
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
+
+@app.route("/")
+def main_page():
+    return render_template("index.html")
+
+@app.route("/rules")
+def rules():
+    return render_template("rules.html")
+
+@app.route("/info")
+def information():
+    return render_template("info.html")
