755cf888eeef7315b2ae556adfc62b671f035643
Bsubs <bsubs02@gmail.com>
2022-04-18 09:09:57 +0800

    Cleaned up ignored files

---
+++ cartographers/app.py
@@ -0,0 +1,22 @@
+from distutils.log import info
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myDB.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+login = LoginManager(app)
+login.login_view = 'login'
+
+import routes, models
+
+# Used to initalize database
+#db.create_all()
