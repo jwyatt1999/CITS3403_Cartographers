a9a9959015fefa81139f177f0bf047b9188c5a75
jwyatt1999 <joshua.wyatt1999@gmail.com>
2022-04-23 15:06:26 +0800

    fixed issues caught by validator

--- game_logic.js
+++ game_logic.js
@@ -193,7 +193,7 @@ function xmur3(str) {
         h = Math.imul(h ^ (h >>> 16), 2246822507);
         h = Math.imul(h ^ (h >>> 13), 3266489909);
         return (h ^= h >>> 16) >>> 0;
-    }
+    };
 }
 
 /**
@@ -208,7 +208,7 @@ function mulberry32(a) {
       t = Math.imul(t ^ t >>> 15, t | 1);
       t ^= t + Math.imul(t ^ t >>> 7, t | 61);
       return ((t ^ t >>> 14) >>> 0) / 4294967296;
-    }
+    };
 }
 
 /**
