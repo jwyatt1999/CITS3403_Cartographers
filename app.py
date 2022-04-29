4856bc8a6d65438e7806824de4654ad679968bb7
jwyatt1999 <joshua.wyatt1999@gmail.com>
2022-04-29 13:21:02 +0800

    changed file structure

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = 'login'
 
-from cartographers import models, routes
+import models, routes
 
 # Used to initalize database
 db.create_all()
