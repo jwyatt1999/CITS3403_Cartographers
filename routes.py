7de7e5a8b1caca9eb5f6c8feb08f9e60c7567229
jwyatt1999 <joshua.wyatt1999@gmail.com>
2022-05-02 17:45:41 +0800

    fix registration redirection

--- routes.py
+++ routes.py
@@ -67,7 +67,7 @@ def register():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for(''))
+        return redirect(url_for('main_page'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
