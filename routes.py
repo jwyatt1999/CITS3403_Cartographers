8863638110c12bc8c071c65ba3860b13b5eb84b7
Bsubs <bsubs02@gmail.com>
2022-05-15 15:20:18 +0800

    Updated queries in /user route

--- routes.py
+++ routes.py
@@ -41,18 +41,18 @@ def credits():
 def user(username):
     user = current_user
     total_avg = Scorecard.query.with_entities(func.avg(Scorecard.score)).filter(Scorecard.uname == user.username).all()
-    total_games = Scorecard.query.filter(Scorecard.uname == user.username).count()
+    total_games = user.scorelists.scorecards.count()
 
     freeplay_avg = Scorecard.query.with_entities(func.avg(Scorecard.score)).filter(Scorecard.uname == user.username).filter(Scorecard.type == 2).all()
-    freeplay_games = Scorecard.query.filter(Scorecard.uname == user.username).filter(Scorecard.type == 2).count()
-    freeplay_highscore = Scorecard.query.filter(Scorecard.type == 2).filter(Scorecard.uname == user.username).order_by(Scorecard.score.desc()).first()
+    freeplay_games = user.scorelists.scorecards.filter(Scorecard.type == 2).count()
+    freeplay_highscore = user.scorelists.scorecards.filter(Scorecard.type == 2).order_by(Scorecard.score.desc()).first()
 
     daily_avg = Scorecard.query.with_entities(func.avg(Scorecard.score)).filter(Scorecard.uname == user.username).filter(Scorecard.type == 1).all()
-    daily_games = Scorecard.query.filter(Scorecard.uname == user.username).filter(Scorecard.type == 1).count()
-    daily_highscore = Scorecard.query.filter(Scorecard.type == 1).filter(Scorecard.uname == user.username).order_by(Scorecard.score.desc()).first()
+    daily_games = user.scorelists.scorecards.filter(Scorecard.type == 1).count()
+    daily_highscore = user.scorelists.scorecards.filter(Scorecard.type == 1).order_by(Scorecard.score.desc()).first()
 
-    scores_daily = Scorecard.query.filter(Scorecard.type == 1).filter(Scorecard.scorelist_id == user.scorelists.id).order_by(Scorecard.date.desc())
-    scores_freeplay = Scorecard.query.filter(Scorecard.type == 2).filter(Scorecard.scorelist_id == user.scorelists.id).order_by(Scorecard.date.desc())
+    scores_daily = user.scorelists.scorecards.filter(Scorecard.type == 1).order_by(Scorecard.date.desc())
+    scores_freeplay = user.scorelists.scorecards.filter(Scorecard.type == 2).order_by(Scorecard.date.desc())
     return render_template('profile_page.html', user=user, score_freeplay = scores_freeplay, score_daily = scores_daily, total_avg=total_avg, total_games=total_games, 
     freeplay_avg=freeplay_avg, freeplay_games=freeplay_games, daily_avg=daily_avg, daily_games=daily_games, freeplay_highscore=freeplay_highscore, daily_highscore=daily_highscore)
 
