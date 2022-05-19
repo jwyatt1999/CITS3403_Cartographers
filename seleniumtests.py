06d585ef7398c44364fea84fd064cc546441f880
jwyatt1999 <joshua.wyatt1999@gmail.com>
2022-05-19 12:21:41 +0800

    removed unnecessary imports

--- seleniumtests.py
+++ seleniumtests.py
@@ -2,9 +2,6 @@ import unittest, os, time
 from app import app, db
 from models import User, Scorecard, Scorelist
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 class SystemTest(unittest.TestCase):
     driver = None
